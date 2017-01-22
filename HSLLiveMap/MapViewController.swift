c5939f6fa8b4bfd6339129c18f81c905c70b4cf9
Joni Nevalainen <joninevalainen@gmail.com>
2017-01-22 17:52:13 +0200

    initial commit

---
+++ HSLLiveMap/MapViewController.swift
@@ -0,0 +1,103 @@
+//
+//  MapViewController.swift
+//  HSLLiveMap
+//
+//  Created by Joni Nevalainen on 22/01/17.
+//  Copyright © 2017 Joni Nevalainen. All rights reserved.
+//
+
+import UIKit
+import CocoaMQTT
+import MapKit
+import Foundation
+
+class MapViewController: UIViewController, CocoaMQTTDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+
+    var points: [String:MKPointAnnotation] = [:]
+    var mqtt: CocoaMQTT!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        mqtt = CocoaMQTT(clientID: "MQTT-LiveIOSApp", host: "mqtt.hsl.fi", port: 1883)
+        mqtt.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        mqtt.connect()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        mqtt.disconnect()
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
+        mqtt.subscribe("/hfp/journey/#")
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        if let data = message.string!.data(using: String.Encoding.utf8) {
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let dict = json as? [String:Any] {
+                    if let vp = dict["VP"] as? [String:Any] {
+                        let vehicleId = vp["veh"] as! String
+                        let longitude = vp["long"] as! Double
+                        let latitude = vp["lat"] as! Double
+                        let line = vp["desi"] as! String
+
+                        if self.points[vehicleId] != nil {
+                            self.map.removeAnnotation(self.points[vehicleId]!)
+                            self.points.removeValue(forKey: vehicleId)
+                        }
+
+                        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                        let p = MKPointAnnotation()
+                        p.coordinate = coordinate
+                        
+                        self.points[vehicleId] = p
+                        
+                        map.addAnnotation(p)
+                    }
+                }
+            } catch {}
+        }
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("subscribed to " + topic)
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("unsubscribed from " + topic)
+    }
+
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+    }
+
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+    }
+
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+    }
+}
+
---
+++ HSLLiveMap/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  HSLLiveMap
+//
+//  Created by Joni Nevalainen on 22/01/17.
+//  Copyright © 2017 Joni Nevalainen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
